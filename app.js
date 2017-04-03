a2e139845cb6de176ef5db33fcce61bfa41129f8
GilbertoHdz <ghernandez.9002@gmail.com>
2017-04-02 20:26:55 -0500

    Nyan & test aproved

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+var index = require('./routes/index')
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -14,6 +16,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
