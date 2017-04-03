fc567172615948aa9e2386726ea530a5e1c68841
GilbertoHdz <ghernandez.9002@gmail.com>
2017-04-02 23:29:30 -0500

    Movie: GET FindAll Test

--- routes/movie.js
+++ routes/movie.js
@@ -5,7 +5,8 @@ var router = express.Router()
 var Movie = {}
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
+router
+.post('/', function(req, res, next) {
   console.log("POST: ", req.body)
   if(!req.body){
     res.status(403).json({error: true, message: 'Body empty'})
@@ -17,6 +18,11 @@ router.post('/', function(req, res, next) {
   Movie[_movie._id] = _movie
 
   res.status(201).json({movie: Movie[_movie._id]})
+})
+
+.get('/', function(req, res, next) {
+  console.log("GET: ", req.body)
+  res.status(200).json({movies: _.values(Movie)})
 })
 
 module.exports = router
--- test/movie.js
+++ test/movie.js
@@ -1,5 +1,6 @@
 "use strict"
 let request = require('supertest-as-promised')
+const _ = require('lodash')
 const api = require('../app')
 const host = api
 
@@ -30,6 +31,68 @@ describe('la ruta de peliculas', function(){
         expect(movie).to.have.property('_id')
         
         done(err)
+      })
+    })
+  })
+
+  describe('una peticion GET', function(){
+    it('deberia obtener todas las peliculas', function(){
+      let movie_id
+      let movie2_id
+
+      let movie = {
+        'title': 'back to the future',
+        'year': '1985'
+      }
+
+      let movie2 = {
+        'title': 'back to the future 2',
+        'year': '1989'
+      }
+
+      request
+        .post('/movie')
+        .set('Acept', 'application/json')
+        .send(movie)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+      .then((res) => {
+        movie_id = res.body.movie._id
+        return request
+          .post('/movie')
+          .set('Acept', 'application/json')
+          .send(movie2)
+          .expect(201)
+          .expect('Content-Type', /application\/json/)
+      })
+      .then((res) => {
+        movie2_id = res.body.movie._id
+        return request
+          .get('/movie')
+          .send(movie2)
+          .expect(201)
+          .expect('Content-Type', /application\/json/)
+      })
+      .then((res) => {
+        let body = req.body
+
+        expect(body).to.have.property('movies')
+        expect(body.movies).to.be.an('array')
+          .and.to.have.length.above(2)
+
+        let movies = body.movies
+        movie = _.find(movies, {_id: movie_id})
+        movie2 = _.find(movies, {_id: movie2_id})
+
+        expect(movie).to.have.property('_id', movie_id)
+        expect(movie).to.have.property('title', 'back to the future')
+        expect(movie).to.have.property('year', '1985')
+        
+        expect(movie2).to.have.property('_id', movie2_id)
+        expect(movie2).to.have.property('title', 'back to the future 2')
+        expect(movie2).to.have.property('year', '1989')
+
+        done()
       })
     })
   })
